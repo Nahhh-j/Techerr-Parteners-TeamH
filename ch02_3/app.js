e8bce4533c4a5d5a07ec7873961a0e9f30679c2f
KimJungYeon <jungyeon9130@kpu.ac.kr>
2022-11-21 21:24:24 +0900

    ch02_2.0-2.3
    ch02_2.0-2.3

---
+++ ch02_3/app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+//const myName = "jungyeon";
+let myName = "jungyeon";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello"+myName);
+
+myName = "Kim";
+console.log("New name is" + myName);
