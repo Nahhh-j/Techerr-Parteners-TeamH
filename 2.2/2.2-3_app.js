10fc10a798f55d6fbb14ff970aeec3a92ad30106
BellYun <95991290+BellYun@users.noreply.github.com>
2022-11-22 01:24:50 +0900

    2.5까지

---
+++ 2.2/2.2-3_app.js
@@ -0,0 +1,20 @@
+console.log(545454545);
+console.log('lalalalalala');
+
+let a = 5; //변수
+let b = 2;
+
+var c = 5; //규칙X
+
+const veryLongVariableName = 0; //상수
+
+let myName = 'nico';
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello' + myName);
+
+myName = 'nicolas';
+
+console.log('your new name is' + myName);
---
+++ 2.5/2.5_app.js
@@ -0,0 +1,5 @@
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const nonsense = [1, 2, 'hello', false, null, true, undefined, 'nico'];
+
+console.log(daysOfWeek, nonsense);
