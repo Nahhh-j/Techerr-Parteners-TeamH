628e378c141f970b25f00d0daf9c2fbc59a382ff
minseok <skyfox001015@naver.com>
2022-11-24 00:41:17 +0900

    2.16 Conditionals

--- 김민석/app.js
+++ 김민석/app.js
@@ -1,18 +1,13 @@
-const calculater = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt('How old are you?'));
 
-alert(calculater.plus(5, 6));
-alert(calculater.divide(6, 2));
-alert(calculater.power(2, 3));
+if (isNaN(age)) {
+  console.log('Please write a number');
+} else if (age < 18) {
+  console.log('You are too young.');
+} else if (age >= 18 && age <= 50) {
+  console.log('You can drink');
+} else if (age > 50 && age <= 80) {
+  console.log('You should exercise.');
+} else if (age > 80) {
+  console.log('You con do whatever you want');
+}
