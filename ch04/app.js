805fdcdbf762bafde5f708ec428495290ffa8dad
KimJungYeon <jungyeon9130@kpu.ac.kr>
2022-12-01 20:50:27 +0900

    add ~04_7
    add ~04_7

--- ch04/app.js
+++ ch04/app.js
@@ -1,25 +1,31 @@
 const loginForm = document.querySelector("#login-form")
 const logininput = document.querySelector("#login-form input")
 const loginButton = loginForm.querySelector("button")
-
+const greeting = document.querySelector("#greeting")
 const link = document.querySelector("a")
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){
     event.preventDefault()
     // 기본동작을 막는것 예를들어, submit 버튼을 눌렀을때 새로고침되는것
-    console.dir(logininput.value)
-    console.log("click!")
     const username = logininput.value
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY, logininput.value)
+    paintGreetings(username)
+}
 
-    if(username === ""){
-        alert("Please Write Yout Name")
-    }else if(username.length > 15){
-        alert("Your name is so long.")
-    }
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY)
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
-function handleLinkClick(event){
-    event.preventDefault()
-    alert("click")
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", onLoginSubmit)
+}else{
+    paintGreetings()
 }
-loginForm.addEventListener("submit", onLoginSubmit)
-link.addEventListener("click",handleLinkClick)
