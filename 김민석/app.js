673ae872916f936c1a927972f7bc65e6383a098a
minseok <skyfox001015@naver.com>
2022-12-02 23:42:19 +0900

    add 4.7

--- 김민석/app.js
+++ 김민석/app.js
@@ -1,9 +1,27 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginBtnClick(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add('hidden');
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintinGreetings(username);
 }
 
-loginForm.addEventListener('submit', onLoginBtnClick);
+function paintinGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove('hidden');
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginBtnClick);
+} else {
+  paintinGreetings(savedUsername);
+}
